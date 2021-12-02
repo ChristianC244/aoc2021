516faf5daaac42b3a11fe754f1a92cb8c8c37286
ChristianC244 <christiancattai@gmail.com>
2021-12-02 18:28:04 +0100

    day02 part 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,7 +1,6 @@
 use std::io::Read;
 use regex::Regex;
 
-
 fn main() {
     let mut input = String::new();
     std::io::stdin().read_to_string(&mut input)
@@ -15,6 +14,7 @@ fn main() {
         .collect();
 
     part_one(&vec);
+    part_two(&vec);
 
 }
 
@@ -27,11 +27,26 @@ fn part_one(vec: &Vec<(String,u32)> ) {
             "forward" => coord = (coord.0 + cmd.1, coord.1),
             "up" => coord = (coord.0, coord.1 - cmd.1),
             "down" => coord = (coord.0, coord.1 + cmd.1),
-            _ => return,
-            
+            _ => return, 
         }
     }
 
     println!("PART ONE\nResult: {}", coord.0*coord.1)
+}
+
+fn part_two(vec: &Vec<(String, u32)>) {
+    let mut coord = (0,0);
+    let mut aim = 0;
+
+    for cmd in vec.iter() {
+        match cmd.0.as_str() {
+            "forward" => coord = (coord.0 + cmd.1, coord.1 + aim*cmd.1),
+            "up" => aim -= cmd.1,
+            "down" => aim += cmd.1,
+            _ => return, 
+        }
+    }
+
+    println!("PART TWO\nResult: {}", coord.0*coord.1)
 }
 
