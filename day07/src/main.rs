58493060c3983b831229f94c3472375ab9d2a7c8
ChristianC244 <christiancattai@gmail.com>
2021-12-07 21:23:15 +0100

    day07 optimized

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -3,65 +3,51 @@ fn main() {
     let mut buf = String::new();
     std::io::stdin().read_to_string(&mut buf).unwrap();
 
-    let input: Vec<usize> = buf.split(",").map(|x| x.parse::<usize>()
+    let input: Vec<i32> = buf.split(",").map(|x| x.parse::<i32>()
         .unwrap())
         .collect();
 
-    let max_len = *input.clone()
-        .iter()
-        .max()
-        .unwrap();
+    part_one_opt(&input);
+    part_two_opt(&input);
+}
+fn fuel_calc(target: i32, crabs: &Vec<i32>) -> i32{
+    
+    let crabs = crabs.clone();
+    crabs.iter()
+        .map(| x | {
+        ((target - x) * (target - x) + (target - x).abs() ) /2
+        })
+        .sum()
+}
 
-    let line = input.iter().fold(vec![0; max_len +1], |mut acc, x| {
-        acc[*x] += 1;
-        acc
+fn part_one_opt(line: &Vec<i32>) {
+    // Calculates median, then sums distances from it. That is the fuel cost
+    println!("PART ONE OPTIMIZED");
+    
+    let mut crab_pos = line.clone();
+    crab_pos.sort_unstable();
+    
+    let median = crab_pos[(crab_pos.len()/2)];
+    
+    let fuel = crab_pos.iter().fold(0, |acc, x| {
+        acc + (*x - median).abs()
     });
 
-    part_one(line.clone());
-    part_two(line);
+    println!("Min fuel cost: {}",fuel);    
 }
 
-fn fuel_calc(dist: i32) -> i32{
-    dist*(dist+1)/2
+fn part_two_opt(line: &Vec<i32>) {
+    // The target position is the mean of all the starting positions ± 1/2 
+
+    let crab_pos = line.clone();
+    let num_crab = line.len() as i32;
+
+    let mean = crab_pos.iter().fold(0, |acc, x| acc + *x) / num_crab;
+
+    let fuel1 = fuel_calc(mean + 1, &crab_pos);
+    let fuel2 = fuel_calc(mean , &crab_pos);
+    let fuel3 = fuel_calc(mean - 1, &crab_pos);
+
+    let fuel = vec![fuel1,fuel2,fuel3];
+    println!("Min fuel cost: {}", fuel.iter().min().unwrap());
 }
-
-fn part_one(line: Vec<i32>) {
-    println!("PART ONE");
-
-    let vec_len = line.len();
-    //For every fish calculate how much it costs to move in each space and sum with others
-    let fuel_cost = line.iter()
-        .enumerate()
-        .fold(vec![0i32;vec_len], | mut cost, (i, val) | {
-        
-        let i = i as i32;
-        cost.iter_mut()
-            .enumerate()
-            .map(|(c_i, c_val)| {
-                *c_val + ( c_i as i32 - i).abs() * *val
-            }).collect()
-    });
-
-    println!("{}", fuel_cost.iter().min().unwrap());
-}
-
-fn part_two(line: Vec<i32>) {
-    println!("PART TWO");
-
-    let vec_len = line.len();
-    //For every fish calculate how much it costs to move in each space and add to others
-    let fuel_cost = line.iter()
-        .enumerate()
-        .fold(vec![0i32;vec_len], | mut cost, (i, val) | {
-        
-        let i = i as i32;
-        cost.iter_mut()
-            .enumerate()
-            .map(|(c_i, c_val)| {
-                *c_val + fuel_calc(( c_i as i32 - i).abs() ) * *val
-            }).collect()
-    });
-
-    println!("{}", fuel_cost.iter().min().unwrap());
-}
-
